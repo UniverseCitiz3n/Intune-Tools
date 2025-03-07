6c79a08aa399812e35e5cb5e4b6d910701878ffa
UniverseCitiz3n <maciej.horbacz@gmail.com>
2025-03-07 15:54:03 +0100

    Update Get-IntuneDevices.ps1 to increment version to 1.0.2 and add release notes for the minor bug fix

--- Scripts/Get-IntuneDevices.ps1
+++ Scripts/Get-IntuneDevices.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 .VERSION
-1.0.1
+1.0.2
 
 .GUID
 e00cc407-4231-4af7-a226-f2a9b28395f3
@@ -42,6 +42,7 @@ https://github.com/UniverseCitiz3n/Intune-Tools
 .RELEASENOTES
     v1.0.0 - Initial version.
     v1.0.1 - Minor bug fix.
+    v1.0.2 - Minor bug fix.
    
 .PRIVATEDATA
 #>
